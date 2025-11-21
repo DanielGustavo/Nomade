add69f6baf48e067da7fe1c4e81066f1153d247f
DanielGustavo <dg520589@gmail.com>
2025-11-20 22:26:56 -0300

    fix LSPs

--- lua/mysetup/lsp.lua
+++ lua/mysetup/lsp.lua
@@ -1,50 +1,7 @@
-local defaultRootMarkers = { '.git' }
-local servers = {}
-
-local mason_registry_ok, mason_registry = pcall(require, 'mason-registry')
-
-function addServer(masonOpt, cmd, filetypes, rootMarkers, customConfig)
-  local server = {
-    name = masonOpt.name,
-    command = cmd,
-    fileTypes = filetypes,
-    rootMarkers = {
-      unpack(defaultRootMarkers),
-      unpack(rootMarkers ~= nil and rootMarkers or {})
-    },
-    config = customConfig ~= nil and customConfig or {}
-  }
-
-  if mason_registry_ok then
-    local package_name = masonOpt.name
-
-    if package_name then
-      local pkg = mason_registry.get_package(package_name)
-
-      if pkg and not pkg:is_installed() then
-        vim.notify("Mason: installing '" .. package_name .. "'... (run ':Mason' to check progress)", vim.log.levels.INFO, { title = "LSP Install" })
-        pkg:install({ version = masonOpt.version })
-      elseif not pkg then
-        vim.notify("Mason: package '" .. package_name .. "' not found.", vim.log.levels.WARN, { title = "LSP Install" })
-      end
-    end
-  end
-
-  table.insert(servers, server)
-
-  vim.lsp.config[server.name] = {
-    cmd = server.command,
-    filetypes = server.fileTypes,
-    root_markers = server.rootMarkers,
-    settings = server.config,
-  }
-end
-
-function enableServers()
-  for i, server in ipairs(servers) do
-    vim.lsp.enable(server.name)
-  end
-end
+local defaultCapabilities = vim.lsp.protocol.make_client_capabilities()
+vim.lsp.config('*', {
+  capabilities = require('blink.cmp').get_lsp_capabilities(defaultCapabilities)
+})
 
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event)
@@ -59,27 +16,35 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
-addServer(
-  { name = 'lua-language-server', version = '3.15.0' },
-  { 'lua-language-server' },
-  { 'lua' },
-  { '.luarc.json' },
-  { Lua = { workspace = { ignoreDir = { "mason/packages" } }},
+local servers_to_configure = {
+  ['lua_ls'] = {
+    settings = {
+      Lua = {
+        workspace = {
+          checkThirdParty = false,
+          ignoreDir = { "mason/packages" },
+        },
+        telemetry = { enable = false },
+      },
+    },
+    root_markers = { '.luarc.json' },
+  },
+
+  ['ts_ls'] = {
+    root_markers = { '.git', 'tsconfig.json', 'jsconfig.json' },
+  },
+
+  ['jsonls'] = {},
+}
+
+require('mason-lspconfig').setup({
+  ensure_installed = vim.tbl_keys(servers_to_configure),
+  automatic_enable = false
 })
 
-addServer(
-  { name = 'typescript-language-server', version = '5.1.2' },
-  { 'typescript-language-server', '--stdio' },
-  { 'typescript', 'javascript' }
-)
+for server, config in pairs(servers_to_configure) do
+  vim.lsp.config(server, config)
+end
 
-addServer(
-  { name = 'prettierd', version = '0.26.2' },
-  { 'prettierd' },
-  { 'angular', 'css', 'flow', 'graphql', 'html', 'json', 'jsx', 'javascript', 'less', 'markdown', 'scss', 'typescript', 'vue', 'yaml' }
-)
-
-addServer({ name = 'jsonlint', version = '1.6.3' }, { 'jsonlint' }, { 'json' })
-addServer({ name = 'eslint_d', version = '14.3.0' }, { 'eslint_d' }, { 'typescript', 'javascript' })
-
-enableServers()
+require('nvim-eslint').setup()
+vim.lsp.enable(vim.tbl_keys(servers_to_configure))
